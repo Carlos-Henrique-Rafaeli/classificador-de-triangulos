d315a57ba1ddf4e34cc7a0554cc2f31d677d705b
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-18 17:10:46 -0300

    Adicionada classe estática ClassificadorDeTriangulos

---
+++ ClassificacaoDeTriangulos.ConsoleApp/ClassificadorDeTriangulos.cs
@@ -0,0 +1,30 @@
+﻿namespace ClassificacaoDeTriangulos.ConsoleApp;
+
+public static class ClassificadorDeTriangulos
+{
+    public static bool MedidasFormamTrianguloValido(int ladoX, int ladoY, int ladoZ)
+    {
+        bool medidasTriangulosValida =
+                ladoX + ladoY > ladoZ &&
+                ladoX + ladoZ > ladoY &&
+                ladoY + ladoZ > ladoX;
+
+        return medidasTriangulosValida;
+    }
+
+    public static string ClassificarTriangulo(int ladoX, int ladoY, int ladoZ)
+    {
+        string tipoTriangulo = "Não classificado";
+
+        if (ladoX == ladoY && ladoY == ladoZ)
+            tipoTriangulo = "Equilátero";
+
+        else if (ladoX != ladoY && ladoY != ladoZ && ladoX != ladoZ)
+            tipoTriangulo = "Escaleno";
+
+        else
+            tipoTriangulo = "Isóceles";
+
+        return tipoTriangulo;
+    }
+}
--- ClassificacaoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificacaoDeTriangulos.ConsoleApp/Program.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace ClassificacaoDeTriangulos.ConsoleApp;
+﻿namespace ClassificacaoDeTriangulos.ConsoleApp;
 
 internal class Program
 {
@@ -16,12 +14,13 @@ internal class Program
 
             ExibirValoresInformados(ladoX, ladoY, ladoZ);
 
-            if (MedidasFormamTrianguloValido(ladoX, ladoY, ladoZ))
+            if (ClassificadorDeTriangulos.MedidasFormamTrianguloValido(ladoX, ladoY, ladoZ))
             {
-                string tipoTriangulo = ClassificarTriangulo(ladoX, ladoY, ladoZ);
+                string tipoTriangulo = ClassificadorDeTriangulos.ClassificarTriangulo(ladoX, ladoY, ladoZ);
 
                 Console.WriteLine($"O triângulo é {tipoTriangulo}!");
             }
+
             else
                 Console.WriteLine("Os valores informados não formam um triângulo válido!");
 
@@ -68,31 +67,5 @@ internal class Program
         Console.WriteLine("Lado Y: " + ladoY);
         Console.WriteLine("Lado Z: " + ladoZ);
         Console.WriteLine("-------------------------------------------\n");
-    }
-
-    static bool MedidasFormamTrianguloValido(int ladoX, int ladoY, int ladoZ)
-    {
-        bool medidasTriangulosValida =
-                ladoX + ladoY > ladoZ &&
-                ladoX + ladoZ > ladoY &&
-                ladoY + ladoZ > ladoX;
-
-        return medidasTriangulosValida;
-    }
-
-    static string ClassificarTriangulo(int ladoX, int ladoY, int ladoZ)
-    {
-        string tipoTriangulo = "Não classificado";
-
-        if (ladoX == ladoY && ladoY == ladoZ)
-            tipoTriangulo = "Equilátero";
-
-        else if (ladoX != ladoY && ladoY != ladoZ && ladoX != ladoZ)
-            tipoTriangulo = "Escaleno";
-
-        else
-            tipoTriangulo = "Isóceles";
-
-        return tipoTriangulo;
     }
 }
