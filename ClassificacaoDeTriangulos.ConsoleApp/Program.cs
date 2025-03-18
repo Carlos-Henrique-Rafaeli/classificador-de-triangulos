23bd49ac5a4bb807fcb6e7888a690ec17e614565
Carlos Henrique Rafaeli <gordinez658@gmail.com>
2025-03-18 15:39:44 -0300

    Implementada versão inicial com menu

--- ClassificacaoDeTriangulos.ConsoleApp/Program.cs
+++ ClassificacaoDeTriangulos.ConsoleApp/Program.cs
@@ -1,10 +1,36 @@
-﻿namespace ClassificacaoDeTriangulos.ConsoleApp
+﻿namespace ClassificacaoDeTriangulos.ConsoleApp;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        while (true)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Clear();
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Classificador de Triângulos");
+            Console.WriteLine("-------------------------------------------");
+
+            Console.Write("Informe o valor do lado X: ");
+            int ladoX = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("Informe o valor do lado Y: ");
+            int ladoY = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Informe o valor do lado Z: ");
+            int ladoZ = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("-------------------------------------------");
+            Console.WriteLine("Valores Informados: ");
+            Console.WriteLine("Lado X: " + ladoX);
+            Console.WriteLine("Lado Y: " + ladoY);
+            Console.WriteLine("Lado Z: " + ladoZ);
+            Console.WriteLine("-------------------------------------------");
+
+            Console.Write("Deseja continuar? (S/N)");
+            string opcaoContinuar = Console.ReadLine()!.ToUpper();
+            if (opcaoContinuar != "S")
+                break;
         }
     }
 }
